728506076a816081e7e9e217c2c8a95d6ce838f2
NebGod11921 <134355572+NebGod11921@users.noreply.github.com>
2024-02-21 16:54:22 +0700

    Update some data

---
+++ GroupProject_InteriorQuotation_NET1714_Group3/Application/IRepositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IRepositories
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User> GetUserByEmailAddressAndPasswordHash(string username, string passwordHash);
+        Task<bool> CheckEmailAddressExisted(string emailaddress);
+        Task<bool> CheckPhoneNumberExited(string phonenumber);
+        Task<IEnumerable<User>> SearchAccountByNameAsync(string name);
+        Task<IEnumerable<User>> SearchAccountByRoleNameAsync(string roleName);
+        Task<IEnumerable<User>> GetSortedAccountAsync();
+    }
+}
---
+++ GroupProject_InteriorQuotation_NET1714_Group3/Infrustructure/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Application;
+using Application.IRepositories;
+using Infrustructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IUserRepository _userRepository;
+
+        public UnitOfWork(AppDbContext dbContext, IUserRepository userRepository)
+        {
+            _dbContext = dbContext;
+            _userRepository = userRepository;
+        }
+
+        public IUserRepository UserRepository => _userRepository;
+
+        public async Task<int> SaveChangeAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
