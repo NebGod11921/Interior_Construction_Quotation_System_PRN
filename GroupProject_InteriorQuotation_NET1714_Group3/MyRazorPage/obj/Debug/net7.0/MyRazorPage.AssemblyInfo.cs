066e534fd2bbbdcf31df4b13e3df3d996df42073
NebGod11921 <134355572+NebGod11921@users.noreply.github.com>
2024-02-24 13:32:55 +0700

    updateinformation

--- GroupProject_InteriorQuotation_NET1714_Group3/MyRazorPage/obj/Debug/net7.0/MyRazorPage.AssemblyInfo.cs
+++ GroupProject_InteriorQuotation_NET1714_Group3/MyRazorPage/obj/Debug/net7.0/MyRazorPage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b6b0665207b1b020c48528eb747ceb790d9498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
