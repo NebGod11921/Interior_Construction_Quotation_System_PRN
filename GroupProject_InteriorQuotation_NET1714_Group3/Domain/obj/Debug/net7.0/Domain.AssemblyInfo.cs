ad8c585d75bcefdd4f7a5d946a5ea760440a3a07
congdanh1z9 <congdangdanh@gmail.com>
2024-02-24 13:39:32 +0700

    cache

--- GroupProject_InteriorQuotation_NET1714_Group3/Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ GroupProject_InteriorQuotation_NET1714_Group3/Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574f0d2b7af48b6a10b34b0145c9cff31e128d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
