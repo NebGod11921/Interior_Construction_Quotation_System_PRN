7dc86c3cde1402f457ac72d566250b71b04a39c5
congdanh1z9 <congdangdanh@gmail.com>
2024-03-13 08:46:55 +0700

    update quotation

---
+++ GroupProject_InteriorQuotation_NET1714_Group3/Infrustructure/Repositories/QuotationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.IRepositories;
+using Domain.Entities;
+using Infrustructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class QuotationRepository: GenericRepository<Quotation>, IQuotationRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public QuotationRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+    }
+}
---
+++ GroupProject_InteriorQuotation_NET1714_Group3/MyRazorPage/Pages/QuotationHistory.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorPage.Pages
+{
+    public class QuotationHistoryModel : PageModel
+    {
+        //private readonly IBooking;
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
