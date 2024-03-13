7b9f07144ff44f2367bad2908dec1db87f90887c
congdanh1z9 <congdangdanh@gmail.com>
2024-03-13 15:11:12 +0700

    cd

--- GroupProject_InteriorQuotation_NET1714_Group3/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ GroupProject_InteriorQuotation_NET1714_Group3/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6dba21224adfc57b2ebbd5deb58ee92a2d6278")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af79bd9a2d4aa48355d26d5f731fd5a2dbfe842c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
