2d017a2d236dfd93e0e99e9c9b8935de3e1757ac
congdanh1z9 <congdangdanh@gmail.com>
2024-03-15 12:30:31 +0700

    cd

--- GroupProject_InteriorQuotation_NET1714_Group3/Infrustructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ GroupProject_InteriorQuotation_NET1714_Group3/Infrustructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccac6c1f2f73082be5c10141b5c2446d5dde46ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
