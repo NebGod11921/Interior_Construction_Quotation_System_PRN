055e288e928247542b815e6624812a3a0bea1b78
congdanh1z9 <congdangdanh@gmail.com>
2024-03-20 05:00:11 +0700

    cd

--- GroupProject_InteriorQuotation_NET1714_Group3/Infrustructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ GroupProject_InteriorQuotation_NET1714_Group3/Infrustructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6ddbb6bdaedcec2d24f625c7e6680af8b7fa78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GroupProject_InteriorQuotation_NET1714_Group3/MyRazorPage/obj/Debug/net7.0/MyRazorPage.AssemblyInfo.cs
+++ GroupProject_InteriorQuotation_NET1714_Group3/MyRazorPage/obj/Debug/net7.0/MyRazorPage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6ddbb6bdaedcec2d24f625c7e6680af8b7fa78")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorPage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
